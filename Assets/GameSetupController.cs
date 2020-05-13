25fc7ca482b99b2afff459e6623ef0d81407bda6
Yiming Zhang <allenzym@gmail.com>
2020-05-12 22:14:29 -0700

    Photon QuickStart

---
+++ Assets/GameSetupController.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+// This script will be added to any multiplayer scene
+public class GameSetupController : MonoBehaviour
+{
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+    }
+}

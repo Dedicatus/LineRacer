db990cbc0f384880685bdc62bc7cfd0f58ebbaeb
Yiming Zhang <allenzym@gmail.com>
2020-05-19 23:25:29 -0700

    merge

--- Assets/Resources/Scripts/GameSetupController.cs
+++ Assets/Resources/Scripts/GameSetupController.cs
@@ -56,21 +56,30 @@ public class GameSetupController : MonoBehaviour
     {
 
         Debug.Log("Creating Player");
-        switch (PhotonNetwork.LocalPlayer.ActorNumber) {
-            case 1:
-                PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player1"), spawnPoints[0].position, Quaternion.identity);
-                break;
-            case 2:
-                PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player2"), spawnPoints[1].position, spawnPoints[1].rotation);
-                break;
-            case 3:
-                PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player3"), spawnPoints[2].position, Quaternion.identity);
-                break;
-            case 4:
-                PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player4"), spawnPoints[3].position, spawnPoints[3].rotation);
-                break;
+        if (PhotonNetwork.IsMasterClient)
+        {
 
         }
+        else
+        {
+            switch (PhotonNetwork.LocalPlayer.ActorNumber)
+            {
+                case 1:
+                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player1"), spawnPoints[0].position, Quaternion.identity);
+                    break;
+                case 2:
+                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player2"), spawnPoints[1].position, spawnPoints[1].rotation);
+                    break;
+                case 3:
+                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player3"), spawnPoints[2].position, Quaternion.identity);
+                    break;
+                case 4:
+                    PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player4"), spawnPoints[3].position, spawnPoints[3].rotation);
+                    break;
+
+            }
+        }
+        
 
         
     }

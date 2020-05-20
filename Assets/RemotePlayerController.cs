0a5aed7c8a1772dcc83f669983e06105b781b380
Yiming Zhang <allenzym@gmail.com>
2020-05-20 01:45:26 -0700

    Update RemotePlayerController.cs

--- Assets/RemotePlayerController.cs
+++ Assets/RemotePlayerController.cs
@@ -59,6 +59,8 @@ public class RemotePlayerController : MonoBehaviour
 
         assignRemotePlayer();
 
+        if (remotePlayer1 == null || remotePlayer2 == null || remotePlayer3 == null || remotePlayer4 == null) { return; }
+
         foreach (GameObject player in playerList)
         {
             Player myPlayerScript = player.GetComponent<Player>();

ca1244ebedc389be30b96a1b6e6c61a0c9a850b5
Oliver Ye <wenzi1996@live.com>
2020-05-19 23:59:22 -0700

    Add Trap and Audio Player

---
+++ Assets/Resources/Scripts/AudioController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AudioController : MonoBehaviour
+{
+     private AudioSource audioSource;
+    [SerializeField] 
+    private AudioClip[] audioClips;
+    private PhotonView myPhotonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPhotonView = GetComponent<PhotonView>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TestPlay();
+       // FooPlay();
+    }
+
+    [PunRPC]
+    void RPC_PlayAudio() {
+        audioSource.clip = audioClips[0];
+        audioSource.Play();
+    }
+
+
+    void TestPlay() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myPhotonView.RPC("RPC_PlayAudio", RpcTarget.AllBuffered);
+        }
+    }
+
+    //void FooPlay() {
+    //    if (Input.GetKeyDown(KeyCode.P))
+    //    {
+    //        audioSource.clip = audioClips[0];
+    //        audioSource.Play();
+    //    }
+    //}
+}

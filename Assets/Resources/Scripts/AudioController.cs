438be0ba8e443a0d32a3977daed84b6271f4d4cd
Syaoran1993 <55966107+Syaoran1993@users.noreply.github.com>
2020-05-21 01:06:10 -0700

    audio bank event

--- Assets/Resources/Scripts/AudioController.cs
+++ Assets/Resources/Scripts/AudioController.cs
@@ -10,11 +10,31 @@ public class AudioController : MonoBehaviour
     private AudioClip[] audioClips;
     private PhotonView myPhotonView;
 
+    [FMODUnity.EventRef]
+    FMOD.Studio.EventInstance playAmbience;
+    FMOD.Studio.EventInstance playMusic;
+    FMOD.Studio.EventInstance playWin;
+    FMOD.Studio.EventInstance playLose;
+    FMOD.Studio.EventInstance playDizzy;
+    FMOD.Studio.EventInstance playFirstSheep;
+    FMOD.Studio.EventInstance playScore;
+    FMOD.Studio.EventInstance playSheep;
+
     // Start is called before the first frame update
     void Start()
     {
         myPhotonView = GetComponent<PhotonView>();
         audioSource = GetComponent<AudioSource>();
+        playAmbience = FMODUnity.RuntimeManager.CreateInstance("event:/ambience");
+        playAmbience.start();
+        playMusic = FMODUnity.RuntimeManager.CreateInstance("event:/bgm");
+        playMusic.start();
+        playWin = FMODUnity.RuntimeManager.CreateInstance("event:/win");
+        playLose = FMODUnity.RuntimeManager.CreateInstance("event:/lose");
+        playDizzy = FMODUnity.RuntimeManager.CreateInstance("event:/dizzy");
+        playFirstSheep = FMODUnity.RuntimeManager.CreateInstance("event:/firstsheep");
+        playScore = FMODUnity.RuntimeManager.CreateInstance("event:/score");
+        playSheep = FMODUnity.RuntimeManager.CreateInstance("event:/sheep");
     }
 
     // Update is called once per frame

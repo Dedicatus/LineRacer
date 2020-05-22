1efb4e41915372318276f81aba4cf8e61eabc9b8
Oliver Ye <wenzi1996@live.com>
2020-05-22 02:30:56 -0700

    Update Trap.cs

--- Assets/Resources/Scripts/Trap.cs
+++ Assets/Resources/Scripts/Trap.cs
@@ -49,33 +49,39 @@ public class Trap : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if (!PhotonNetwork.isMasterClient) { return; }
-        if (other.gameObject.tag == "Player" && isActive) {
-            myAnimator.SetBool("isIdle", false);
-            myAnimator.SetBool("isOpen", false);
-            myAnimator.SetBool("isClose", true);
-            Vector3 p = other.transform.position;
-            this.transform.position = new Vector3(p.x, originPosition.y, p.z);
-            isActive = false;
-            other.gameObject.GetComponent<Player>().isStun = true;
-            other.gameObject.GetComponent<Player>().stunTime = stunTime;
-            curPlayer = other.gameObject;
-            myAudioController.playDizzy.start();
-           // myAudioController.playDizzy.release();
+        if (PhotonNetwork.isMasterClient)
+        {
+            if (other.gameObject.tag == "Player" && isActive)
+            {
+                myAnimator.SetBool("isIdle", false);
+                myAnimator.SetBool("isOpen", false);
+                myAnimator.SetBool("isClose", true);
+                Vector3 p = other.transform.position;
+                this.transform.position = new Vector3(p.x, originPosition.y, p.z);
+                isActive = false;
+                other.gameObject.GetComponent<Player>().isStun = true;
+                other.gameObject.GetComponent<Player>().stunTime = stunTime;
+                curPlayer = other.gameObject;
+                myAudioController.playDizzy.start();
+                // myAudioController.playDizzy.release();
 
+            }
         }
     }
 
     public void OnTriggerExit(Collider other)
     {
-        if (!PhotonNetwork.isMasterClient) { return; }
+        if (PhotonNetwork.isMasterClient)
+        {
 
-        if (other.gameObject == curPlayer && !isCoolDown) {
-            myAnimator.SetBool("isClose", false);
-            myAnimator.SetBool("isOpen", true);
-            
-            this.transform.position = originPosition;
-            isCoolDown = true;
+            if (other.gameObject == curPlayer && !isCoolDown)
+            {
+                myAnimator.SetBool("isClose", false);
+                myAnimator.SetBool("isOpen", true);
+
+                this.transform.position = originPosition;
+                isCoolDown = true;
+            }
         }
     }
 }
